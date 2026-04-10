43332590cd4b712e02e5994bd752985e545e3167
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2026-04-10 09:50:25 -0300

    Add files via upload

---
+++ Ponteiros/Endereco_Input.c
@@ -0,0 +1,19 @@
+/* Escreva um programa que contenha duas variaveis inteiras. Leia essas vari ´ aveis do ´
+teclado. Em seguida, compare seus enderec¸os e exiba o conteudo do maior enderec¸o. */
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    scanf("%i", &n1);
+    scanf("%i", &n2);
+
+    int *p1 = &n1, *p2 = &n2;
+
+    if (p1 > p2) {
+        printf("%i\n", *p1); 
+    } else {
+        printf("%i\n", *p2);
+    }
+    return 0;
+}
