362dc343c9a4d382474676288d0e916b9d617a00
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2026-05-07 19:36:04 -0300

    Add files via upload

---
+++ Structs/Struct_Vendas.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+typedef struct Venda
+{
+	int pecas;
+	float preco;
+
+} Venda;
+
+Venda TotalVendas( Venda c,  Venda d);
+
+int main()
+{
+	Venda a, b, total;
+	printf("Venda A\n=========\n");
+	printf("Insira o numero de pecas: ");
+	scanf("%i",&a.pecas);
+	printf("Insira o preco :");
+	scanf("%f",&a.preco);
+	printf("Venda B\n=========\n");
+	printf("Insira o numero de pecas: ");
+	scanf("%i",&b.pecas);
+	printf("Insira o preço :");
+	scanf("%f",&b.preco);
+
+	total=TotalVendas(a,b); // estruturas como argumento e retorno
+
+	printf("\n\n Venda Total\n=========\n");
+	printf("\nTotal de Pecas: %i",total.pecas);
+	printf("\nPreco total : %.2f\n",total.preco);
+	return 0;
+}
+
+Venda TotalVendas(Venda c, Venda d)
+{
+    Venda t;
+	t.pecas=c.pecas+d.pecas;
+	t.preco=c.preco+d.preco;
+	return t;
+}
