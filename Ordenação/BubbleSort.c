5b1f15e1d53478d794417c3c11796d724a9bebe5
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2026-06-09 11:19:47 -0300

    Add files via upload

---
+++ Ordenação/BubbleSort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+void bubble_sort(int a[], int length){
+	
+	bool swapped = false;
+	int i = 0;
+	do{
+		swapped = false;
+		for(int j =0; j < (length - 1 - i); j++){
+			if (a[j] > a[j + 1]){
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+				swapped = true;
+			}
+		}
+	} while(swapped);
+	i++;
+	
+	for(i = 0; i < length; i++){
+		printf("a[%d] = %d\n", i, a[i]);
+		
+	}
+}
+
+int main(void){
+	int a[] = {1, 7, 3, 9, 0, 2, 3, 5, 8, 6};
+	
+	int length = 10;
+	
+	
+	
+	bubble_sort(a, length);
+	return 0;
+}
